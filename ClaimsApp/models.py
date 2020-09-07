3801738c1ba45578ecb62e48e6bc845319179319
Bharat <bezawadabharat@gmail.com>
2020-09-07 13:15:17 +0530

    first commit

--- ClaimsApp/models.py
+++ ClaimsApp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Claims(models.Model):
-    Insured_Name = models.CharField(max_length=20)
-    Buyer_name = models.CharField(max_length=20)
+    Insured_Name = models.CharField(max_length=50)
+    Buyer_name = models.CharField(max_length=50)
     PAN = models.CharField(max_length=20)
     Default_Amt = models.CharField(max_length=20)
     Limit_Type = models.CharField(max_length=20, default=0)
